3abc528cb98ff96a77fec6e7d887bdafc67e485d
BrandynTosten <101216503+BrandynTosten@users.noreply.github.com>
2022-03-08 13:33:35 -0700

    Add files via upload

---
+++ AccountingSystem/AccountingSystem/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace AccountingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.UserBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.Location = new System.Drawing.Point(365, 180);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(75, 23);
+            this.Loginbtn.TabIndex = 0;
+            this.Loginbtn.Text = "button1";
+            this.Loginbtn.UseVisualStyleBackColor = true;
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // UserBox
+            // 
+            this.UserBox.Location = new System.Drawing.Point(314, 60);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(172, 20);
+            this.UserBox.TabIndex = 1;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(314, 120);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(172, 20);
+            this.PassBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.UserBox);
+            this.Controls.Add(this.Loginbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Loginbtn;
+        private System.Windows.Forms.TextBox UserBox;
+        private System.Windows.Forms.TextBox PassBox;
+    }
+}
+
---
+++ AccountingSystem/AccountingSystem/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("server=localhost;database=AccountingSys;UID=sa;password=123456789");
+                SqlCommand cmd = new SqlCommand("select * from UserLogins where UserName=@UserName and Password=@Password", con);
+                cmd.Parameters.AddWithValue("@UserName", UserBox.Text);
+                cmd.Parameters.AddWithValue("@Password", PassBox.Text);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                con.Open();
+                con.Close();
+
+                if (dt.Rows.Count > 0)
+                {
+                    SettingsForm WelcomePage = new SettingsForm(dt.Rows[0][1].ToString());
+
+                    Hide();
+                    WelcomePage.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong user info");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
